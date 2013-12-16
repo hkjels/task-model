59692952aaf2aa7f6bd278f5219d3b64093d6062
Henrik Kjelsberg <hkjels@me.com>
2013-12-16 10:06:36 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,24 @@
+
+# task-model
+
+  Task-model compatible with `scrumcard-view`.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install hkjels/task-model
+
+## API
+
+```JavaScript
+var task = new Task({
+  "id": 1,
+  "title": "Move the lawn",
+  "assignee": "Henrik Kjelsberg",
+  "email": "hkjels@me.com",
+  "priority": "medium",
+  "estimate": 3
+});
+```
+
---
+++ index.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var model = require('model')
+  , gravatar = require('gravatar-component')
+  , isFibonacci = require('is-fibonacci');
+
+/**
+ * Check if string is a priority-string.
+ *
+ * @param {String} str
+ */
+
+function isPriority(str) {
+  var priorities = ['very-low', 'low', 'medium', 'high', 'very-high'];
+  return priorities.indexOf(str);
+}
+
+/**
+ * Task.
+ */
+
+var Task = model('Task')
+  .attr('id', { required: true, type: 'number' })
+  .attr('title', { required: true, type: 'string' })
+  .attr('assignee', { type: 'string' })
+  .attr('email', { type: 'string' })
+  .attr('priority', { type: 'string', validate: isPriority })
+  .attr('estimate', { type: 'number', min: 0, max: 13, validate: isFibonacci });
+
+/**
+ * Assignee photo.
+ */
+
+Task.prototype.photo = function() {
+  return gravatar.url(this.email, {
+    "d": "retro",
+    "r": "g"
+  });
+};
+
+/**
+ * Expose `Task`.
+ */
+
+module.exports = Task;
+
